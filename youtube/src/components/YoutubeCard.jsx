069b5bafdc35648a860495f209155b5344df6b3a
tsetse0725 <tse9406@gmail.com>
2025-04-14 16:45:58 +0800

    update hiiw.

--- youtube/src/components/YoutubeCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function YoutubeCard({ image, icon, title, author, time }) {
-  return (
-    <div>
-      <YoutubeImage src={src} alt={title} />
-      <YoutubeText title={title} author={author} />
-    </div>
-  );
-}
